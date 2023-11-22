973e24671a14f764eec7c4b0584c40c85cd0d6d1
Omobolaji Arigbede <arigbedeomobolaji@gmail.com>
2023-11-22 14:41:04 +0100

    Backend and frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+const App: React.FC = () => {
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Button type="primary">Button</Button>
+		</>
+	);
+};
+export default App;
