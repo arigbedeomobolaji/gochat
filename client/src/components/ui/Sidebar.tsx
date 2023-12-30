bce9b0c67942df5127769bb4ce00c215feaab788
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2023-12-30 15:25:57 +0100

    response sidebar achieved

--- client/src/components/ui/Sidebar.tsx
+++ client/src/components/ui/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useMediaQuery } from "react-responsive";
 import SidebarHeader from "../SidebarHeader";
 import { AppContext, AppContextType } from "@src/screens/Home";
 import { homepageData } from "@src/utils/data";
@@ -11,11 +12,13 @@ export default function Sidebar() {
   const currentMenuData = homepageData.find(
     (datum) => datum.label === currentMenu
   );
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
+  console.log(isTabletOrMobile);
   return (
     <div
       className={`bg-gray-800 p-3 px-5 pt-5 ${
-        sidebarOpen
-          ? "block min-w-[250px] absolute top-0 bottom-0 left-10 md:left-14 p-10"
+        sidebarOpen && isTabletOrMobile
+          ? "block min-w-[300px] absolute top-0 bottom-0 left-10 md:left-14 p-10"
           : "hidden"
       }  xl:flex flex-col flex-1 flex-grow rounded-tl-2xl text-gray-100`}
     >
