6b046f6bc154b6f115905436d462d85d38a6e392
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2024-01-08 17:11:27 +0100

    done with active/inactive implementation

--- client/src/components/ui/FindFriend.tsx
+++ client/src/components/ui/FindFriend.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { UserOutlined, UserAddOutlined } from "@ant-design/icons";
 import { AppContext, AppContextType, UserStatus } from "@src/screens/Home";
 import { Avatar } from "antd";
